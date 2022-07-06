b0c433ac53f7432f186db6ef8d4b70e9361db132
CodeXz <102515167+CodeXz0@users.noreply.github.com>
2022-07-07 00:11:35 +0800

    Update images_api.php
    更新bing每日一图

--- images_api.php
+++ images_api.php
@@ -2,32 +2,43 @@
 /*
 作者：CodeXz/举个栗子
 2022.3.27
+2022.07.07
 
 */
-error_reporting(E_ALL ^ E_NOTTCE);//迫不得已，后续改进ing
-$img_jpg = glob("111/*.{jpg}",GLOB_BRACE);//jpe文件夹随机抽取//111为目录
-$img_png = glob("222/*.{png}",GLOB_BRACE);//png文件夹随机抽取//222为目录
-$img_all = glob("assets/background/*.{jpg,png}",GLOB_BRACE);//全部文件夹随机抽取
-$img_j = array_rand($img_jpg);
-$img_p = array_rand($img_png);
+#$img_png = glob("222/*.{png}",GLOB_BRACE);//png文件夹随机抽取
+$img_all = glob("assets/background/*.{png,jpg}",GLOB_BRACE);//全部文件夹随机抽取
+//
+
+$str = file_get_contents('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
+$str = json_decode($str,true);
+$bingurl = 'https://cn.bing.com'.$str['images'][0]['url'];
+$bingurl4k = 'https://cn.bing.com'.$str['images'][0]['urlbase'].'_UHD.jpg';
+//
+#$img_p = array_rand($img_png);
 $img_all_in = array_rand($img_all);
-$jpg = $img_jpg[$img_j];
-$jpg = $img_png[$img_p];
-$jpg = $img_all[$img_all_in];
+//
+//
+$png = $img_png[$img_p];
+$all_in = $img_all[$img_all_in];
+//
 $type = $_GET['xz'];
 switch ($type)
 {
     case 'all':
-    header("Location:".$img_all);
+    header("Location:".$all_in);
     break;
-
-    case 'all':
-    header("Location:".$jpg);
-    break;
-
-    case 'all':
+    
+    case 'png':
     header("Location:".$png);
     break;
+    
+    case 'bing':
+    header("Location:".$bingurl);
+    break;
+    
+    case 'bing4K':
+    header("Location:".$bingurl4k);
+    break;
 }
-echo('[获取pc图片请输入]:?xz=all')
+echo('[获取图片请输入] ：?xz=all <br/> [获取bing每日一图请输入] ：?xz=bing <br/> [获取bing每日一图4K请输入] ：?xz=bing4K')
 ?>
